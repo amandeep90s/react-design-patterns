d52612b8e4f7646ad2317d4c3ec307250df9964f
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-21 13:02:52 +0530

    feat: Screen splitter enhancement

---
+++ 01-layout-components/src/components/split-screen.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Panel = styled.div`
+  flex: ${(props) => props.flex};
+`;
+
+export const SplitScreen = ({ children, leftWidth = 1, rightWidth = 1 }) => {
+  const [left, right] = children;
+
+  return (
+    <Container>
+      <Panel flex={leftWidth}>{left}</Panel>
+
+      <Panel flex={rightWidth}>{right}</Panel>
+    </Container>
+  );
+};
+
+SplitScreen.propTypes = {
+  children: PropTypes.node.isRequired,
+  leftWidth: PropTypes.number,
+  rightWidth: PropTypes.number,
+};
