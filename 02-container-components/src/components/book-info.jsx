9b5d36ee4d6607152b9d9a37845cde35826e8ba7
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-21 17:04:31 +0530

    feat: Add express and styled-components dependencies

---
+++ 02-container-components/src/components/book-info.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+export const BookInfo = ({ book }) => {
+  const { name, price, title, pages } = book || {};
+
+  return book ? (
+    <>
+      <h3>{name}</h3>
+      <p>Price: {price}</p>
+      <h3>Title: {title}</h3>
+      <p>Number of Pages: {pages}</p>
+    </>
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
+
+BookInfo.propTypes = {
+  book: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.number,
+    title: PropTypes.string,
+    pages: PropTypes.number,
+  }),
+};
